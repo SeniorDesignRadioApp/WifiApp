78c7fec69ff51c78f9b422147ae38d831643c232
Mike Fiore <mfjord02@gmail.com>
2012-11-09 09:34:36 -0600

    app not has a scroll, outputs mac address instead of SSID, shows all networks, then final ones so logic can be verified

--- src/seniordesign/wifi/WiFiScanReceiver.java
+++ src/seniordesign/wifi/WiFiScanReceiver.java
@@ -9,11 +9,13 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.net.wifi.ScanResult;
+import android.util.Log;
 
 
 public class WiFiScanReceiver extends BroadcastReceiver {
 
 	WifiApp wifiapp;
+	private final static String TAG = "WifiApp";
 	
 	private final static ArrayList<Integer> channels = new ArrayList<Integer> (Arrays.asList(0, 2412, 2417, 2422, 2427, 2432, 2437, 2442, 2447, 2452, 2457, 2462));
 	
@@ -31,15 +33,45 @@ public class WiFiScanReceiver extends BroadcastReceiver {
 		String tmp = "";
 		ScanResult sr;
 		Iterator<ScanResult> it = results.iterator();
+		ScanResult channel_info[] = new ScanResult[12];
+		
+		for (int i = 1; i < 12; i++)
+		{
+			channel_info[i] = null;
+		}
+		
+		str += "all channels\n";
 		
 		while (it.hasNext())
 		{
 			sr = it.next();
-			tmp = channels.indexOf(Integer.valueOf(sr.frequency)) + " " + sr.level + " " + sr.SSID + "\n";
-			str += tmp;
+			str += channels.indexOf(Integer.valueOf(sr.frequency)) + " " + sr.level + " " + sr.BSSID + "\n";
+			int channel = channels.indexOf(Integer.valueOf(sr.frequency));
+
+			if (channel_info[channel] == null)
+			{
+				channel_info[channel] = sr;
+			}
+			else
+			{
+				if (channel_info[channel].level < sr.level)
+				{
+					channel_info[channel] = sr;
+				}
+			}
+		}
+		
+		str += "\n\nfinal results\n";
+		
+		for (int i = 1; i < 12; i++)
+		{
+			if (channel_info[i] != null)
+			{
+				tmp = channels.indexOf(Integer.valueOf(channel_info[i].frequency)) + " " + channel_info[i].level + " " + channel_info[i].BSSID + "\n";
+				str += tmp;
+			}
 		}
 		
 		wifiapp.changeText(str);
-		
 	}
 }
